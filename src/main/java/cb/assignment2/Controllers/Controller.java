67c8e428c9583cecb61ca4445b7e63b34772cc25
Christian <200390696@student.georgianc.on.ca>
2023-06-10 22:13:36 -0400

    Final

--- src/main/java/cb/assignment2/Controllers/Controller.java
+++ src/main/java/cb/assignment2/Controllers/Controller.java
@@ -9,21 +9,149 @@ package cb.assignment2.Controllers;
 import cb.assignment2.Models.Course;
 import cb.assignment2.Models.Program;
 import cb.assignment2.Models.Student;
+import cb.assignment2.Views.ViewCourse;
+import cb.assignment2.Views.ViewProgram;
+import cb.assignment2.Views.ViewStudent;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Controller {
 
     /**
      * Controller method. Interfaces between models and views.
+     *
+     * Using one controller for all views, as we discussed in class.
      */
+
+    private Student student;
+    private Course course;
+    private Program program;
+
+    private ViewStudent viewStudent;
+    private ViewCourse viewCourse;
+    private ViewProgram viewProgram;
 
     /**
-     * Models
+     * Default constructor. Accepts one of each model, one of each view.
+     * @param student
+     * @param course
+     * @param program
+     * @param viewStudent
+     * @param viewCourse
+     * @param viewProgram
+     */
+    public Controller(Student student,
+                      Course course,
+                      Program program,
+                      ViewStudent viewStudent,
+                      ViewCourse viewCourse,
+                      ViewProgram viewProgram) {
+        this.student = student;
+        this.course = course;
+        this.program = program;
+        this.viewStudent = viewStudent;
+        this.viewCourse = viewCourse;
+        this.viewProgram = viewProgram;
+    }
+
+    /**
+     * Getters & setters
+     * @return
      */
 
+    public Student getStudent() {
+        return student;
+    }
 
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Program getProgram() {
+        return program;
+    }
+
+    public void setProgram(Program program) {
+        this.program = program;
+    }
+
+    public ViewStudent getViewStudent() {
+        return viewStudent;
+    }
+
+    public void setViewStudent(ViewStudent viewStudent) {
+        this.viewStudent = viewStudent;
+    }
+
+    public ViewCourse getViewCourse() {
+        return viewCourse;
+    }
+
+    public void setViewCourse(ViewCourse viewCourse) {
+        this.viewCourse = viewCourse;
+    }
+
+    public ViewProgram getViewProgram() {
+        return viewProgram;
+    }
+
+    public void setViewProgram(ViewProgram viewProgram) {
+        this.viewProgram = viewProgram;
+    }
+
+    /**
+     * Methods to interact with the models.
+     *
+     * These are kind of redundant in this context. It would be simpler to just access directly through the controller
+     * eg. from Main method call: Controller.getModel.setName(name)
+     * than to use these methods
+     * Controller.setStudentName(name),
+     * and would not have this redundant code. This is how it was done in the example, so I am replicating it here
+     * for demonstrative purposes.
+     *
+     * For that reason, I am not creating a method to access every instance variable. I am creating only a few to
+     * demonstrate.
+     */
+
+    public void setStudentProgram(Program program){
+        student.setStudentProgram(program); //You can see the redundancy I am referring to here
+    }
+
+    public void setStudentCourses(ArrayList<Course> courses){
+        student.setStudentCourses(courses);
+    }
+
+    public void setProgramCourses(ArrayList<Course> courses){
+        program.setProgramCourses(courses);
+    }
+
+    public void setCourseName(String name){
+        course.setCourseName(name);
+    }
+
+    /**
+     * Methods to interface with the views
+     */
+
+    public void updateStudentView(){
+        ViewStudent.printStudent(student);
+    }
+
+    public void updateCourseView(){
+        ViewCourse.printCourse(course);
+    }
+
+    public void updateProgramView(){
+        ViewProgram.printProgram(program);
+    }
 
 
 }
--- src/main/java/cb/assignment2/Main.java
+++ src/main/java/cb/assignment2/Main.java
@@ -6,6 +6,7 @@
 
 package cb.assignment2;
 
+import cb.assignment2.Controllers.Controller;
 import cb.assignment2.Models.Course;
 import cb.assignment2.Models.Program;
 import cb.assignment2.Models.Student;
@@ -24,32 +25,49 @@ public class Main {
     public static void main(String[] args) {
         //Instantiate some students
         Student student1 = new Student(200390696, 25, "Steve", "Smith");
-        Student student2 = new Student(200390697, 23, "Bob", "Baker");
-        Student student3 = new Student(200390698, 27, "Martin", "McDonald");
 
         //Instantiate some courses
-        Course course1 = new Course(0001, "Advanced Java", "Vivek Ahuja", "E208");
+        Course course1 = new Course(1, "Advanced Java", "Vivek Ahuja", "E208");
         Course course2 = new Course(0002, "Javascript", "Hossein Hesami", "K218");
-        Course course3 = new Course(0003, "ASP.NET", "Sean McKinnon", "A222");
 
-        //Instnatiate some programs
-        Program program1 = new Program(3, true, "Computer Programming & Analysis", new ArrayList<>(Arrays.asList(course1, course2, course3)));
-        Program program2 = new Program(4, false, "Business Administration", new ArrayList<>(Arrays.asList(course1, course2, course3)));
-        Program program3 = new Program(1, false, "Finance", new ArrayList<>(Arrays.asList(course1, course2, course3)));
+        //Instantiate some programs
+        Program program1 = new Program(3, true, "Computer Programming & Analysis", new ArrayList<>(Arrays.asList(course1)));
 
-        //Demonstrate controllers
+        //Instantiate a new Controller
+        Controller controller1 = new Controller(student1, course1, program1, new ViewStudent(), new ViewCourse(), new ViewProgram());
 
-        //Demonstrate views
-        ViewStudent.printStudent(student1);
-        ViewStudent.printStudent(student2);
-        ViewStudent.printStudent(student3);
+        //Call the views
+        System.out.println(" "); //Create a new line
+        controller1.updateStudentView();
 
-        ViewCourse.printCourse(course1);
-        ViewCourse.printCourse(course2);
-        ViewCourse.printCourse(course3);
+        System.out.println(" "); //Create a new line
+        controller1.updateCourseView();
 
-        ViewProgram.printProgram(program1);
-        ViewProgram.printProgram(program2);
-        ViewProgram.printProgram(program3);
+        System.out.println(" "); //Create a new line
+        controller1.updateProgramView();
+
+        System.out.println(" "); //Create a new line
+
+        //Call some of the methods to interact with the models
+        System.out.println(" "); //Create a new line
+        controller1.setStudentCourses(new ArrayList<Course>(Arrays.asList(course1, course2)));
+
+        System.out.println(" "); //Create a new line
+        controller1.setCourseName("Even More Advanced Java");
+
+        System.out.println(" "); //Create a new line
+        controller1.setProgramCourses(new ArrayList<Course>(Arrays.asList(course1, course2)));
+
+        System.out.println(" "); //Create a new line
+
+        //Call the views to demonstrate the controller has updated the models
+        System.out.println(" "); //Create a new line
+        controller1.updateStudentView();
+
+        System.out.println(" "); //Create a new line
+        controller1.updateCourseView();
+
+        System.out.println(" "); //Create a new line
+        controller1.updateProgramView();
     }
 }
